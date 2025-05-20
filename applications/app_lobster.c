483c9b20950aea10732f52a1868751260fcb0c8d
DaandeGroot <chanterheld@gmail.com>
2025-05-20 20:42:09 +0200

    start on more logging, still need to figure out the right values

--- applications/app_lobster.c
+++ applications/app_lobster.c
@@ -80,6 +80,7 @@ static THD_FUNCTION(my_thread, arg) {
 
 	is_running = true;
 
+    uint32_t controller_id = app_get_configuration()->controller_id;
 
     for(;;) {
 		// Check if it is time to stop.
@@ -90,27 +91,94 @@ static THD_FUNCTION(my_thread, arg) {
 
 //		timeout_reset(); // Reset timeout if everything is OK.
 
-		// Run your logic here. A lot of functionality is available in mc_interface.h.
-        mc_fault_code fault = mc_interface_get_fault();  //29 faults -> 5 bit min
-        float motor_current =  mcpwm_foc_get_tot_current_directional_filtered(); // +-100A => 200A range @0.1A resolution => 11bit min
-        float rpm = mcpwm_foc_get_rpm(); //+-300k => 600k @ 100rpm range => 13 bits min
-        float temp_mosfet = mc_interface_temp_fet_filtered(); //-50 +150 => 200degr @ 1deg => 8 bit
-        float amp_hours = mc_interface_get_amp_hours(false); // 0 to 100 @ .1 => 1000 => 10 bits
-        uint32_t controller_id = app_get_configuration()->controller_id;
+        // From data:
+        {
+            uint8_t msg_data[8];
+            int32_t index = 0;
 
-        uint8_t msg_data[8];
-        int32_t index = 0;
+            float volt_d = mcpwm_foc_get_vd();
+            utils_truncate_number(&volt_d, -100.0, 100.0);
+            buffer_append_float16(msg_data, volt_d, 300.0, &index);
 
-        buffer_append_float16(msg_data, motor_current, 200.0, &index);
-        buffer_append_float16(msg_data, rpm / 10.0, 1, &index);
-        buffer_append_uint16(msg_data, (uint16_t)(amp_hours * 200.0), &index); //bc unsigned float
-        msg_data[index++] = (uint8_t)(temp_mosfet + 51.0);
-        msg_data[index++] = fault;
+            float volt_q = mcpwm_foc_get_vq();
+            utils_truncate_number(&volt_q, -100.0, 100.0);
+            buffer_append_float16(msg_data, volt_q, 300.0, &index);
+
+            float curr_d = mcpwm_foc_get_id();
+            utils_truncate_number(&curr_d, -100.0, 100.0);
+            buffer_append_float16(msg_data, curr_d, 300.0, &index);
+
+            float curr_q = mcpwm_foc_get_iq();
+            utils_truncate_number(&curr_q, -100.0, 100.0);
+            buffer_append_float16(msg_data, curr_q, 300.0, &index);
+
+            comm_can_transmit_sid(controller_id | 0x0400, msg_data, index);
+        }
+        {
+            uint8_t msg_data[8];
+            int32_t index = 0;
+
+            float volt_in = mc_interface_get_input_voltage_filtered();
+            utils_truncate_number(&volt_in, -100.0, 100.0);
+            buffer_append_float16(msg_data, volt_in, 300.0, &index);
+
+            float curr_motor = mc_interface_get_tot_current_filtered();
+            utils_truncate_number(&curr_motor, -100.0, 100.0);
+            buffer_append_float16(msg_data, curr_motor, 300.0, &index);
+
+            float erpm = mc_interface_get_rpm();
+            utils_truncate_number(&erpm, -325000.0, 325000.0);
+            buffer_append_float16(msg_data, erpm / 10, 1.0, &index);
+
+            float duty = mc_interface_get_duty_cycle_now();
+            utils_truncate_number(&duty, -100.0, 100.0);
+            buffer_append_float16(msg_data, duty, 300.0, &index);
+
+            comm_can_transmit_sid(controller_id | 0x0500, msg_data, index);
+        }
+        {
+            uint8_t msg_data[8];
+            int32_t index = 0;
+
+            float ah_used = mc_interface_get_amp_hours(false);
+            utils_truncate_number(&ah_used, 0.0, 325.0);
+            buffer_append_uint16(msg_data, (uint16_t)(ah_used * 200.0), &index); //bc unsigned float
+
+            mc_fault_code fault = mc_interface_get_fault();
+            msg_data[index++] = fault;
+
+            float temp_mosfet = mc_interface_temp_fet_filtered();
+            msg_data[index++] = (uint8_t)(temp_mosfet + 51.0);
+
+            comm_can_transmit_sid(controller_id | 0x0600, msg_data, index);
+        }
+
+//        data.ah_used = mc_interface_get_amp_hours(false);
+//        data.fault_code = mc_interface_get_fault();
+//        data.temp_mos_max = mc_interface_temp_fet_filtered();
 
 
-        comm_can_transmit_sid(controller_id | 0x0300, msg_data, 8);
+        // Run your logic here. A lot of functionality is available in mc_interface.h.
+//        mc_fault_code fault = mc_interface_get_fault();  //29 faults -> 5 bit min
+//        float motor_current =  mcpwm_foc_get_tot_current_directional_filtered(); // +-100A => 200A range @0.1A resolution => 11bit min
+//        float rpm = mcpwm_foc_get_rpm(); //+-300k => 600k @ 100rpm range => 13 bits min
+//        float temp_mosfet = mc_interface_temp_fet_filtered(); //-50 +150 => 200degr @ 1deg => 8 bit
+//        float amp_hours = mc_interface_get_amp_hours(false); // 0 to 100 @ .1 => 1000 => 10 bits
+//
+//
+//        uint8_t msg_data[8];
+//        int32_t index = 0;
+//
+//        buffer_append_float16(msg_data, motor_current, 200.0, &index);
+//        buffer_append_float16(msg_data, rpm / 10.0, 1, &index);
+//        buffer_append_uint16(msg_data, (uint16_t)(amp_hours * 200.0), &index); //bc unsigned float
+//        msg_data[index++] = (uint8_t)(temp_mosfet + 51.0);
+//        msg_data[index++] = fault;
+//
+//
+//        comm_can_transmit_sid(controller_id | 0x0300, msg_data, 8);
 
-		chThdSleepMilliseconds(5); //200 hz
+		chThdSleepMilliseconds(9); //111 hz -> backbone will be 100hz
 	}
 }
 
